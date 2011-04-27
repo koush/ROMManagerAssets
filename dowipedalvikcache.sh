11e19e5f7d5ba6754894d19fb0cd0c4aed89ce6c
Koushik Dutta <koushd@gmail.com>
2011-04-26 23:49:38 -0700

    new resources for allowing backup of current rom before restore. wipe dalvik cache.

---
+++ dowipedalvikcache.sh
@@ -0,0 +1,6 @@
+#!/sbin/sh
+for partition in data cache system sd-ext
+do
+    mount $partition
+    rm -rf /$partition/dalvik-cache
+done
---
+++ preparewipedalvikcache.sh
@@ -0,0 +1,3 @@
+cd /data/data/com.koushikdutta.rommanager/files
+cat dowipedalvikcache.sh > /cache/dowipedalvikcache.sh
+chmod 755 /cache/dowipedalvikcache.sh
