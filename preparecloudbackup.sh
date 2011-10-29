82a4032645bd56c080cdf1e8f0fac7d3406414cf
Koushik Dutta <koushd@gmail.com>
2011-10-28 17:24:00 -0700

    remove junk

--- preparecloudbackup.sh
+++
@@ -1,7 +0,0 @@
-cd /data/data/com.koushikdutta.rommanager/files
-cat dedupe > /cache/dedupe
-chmod 755 /cache/dedupe
-
-cat docloudbackup.sh > /cache/docloudbackup.sh
-chmod 755 /cache/docloudbackup.sh
-
